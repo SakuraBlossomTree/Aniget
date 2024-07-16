4753faee260b8fce082677c01da618b42415d36f
SakuraBlossomTree <yohib.hussain@gmail.com>
2024-07-16 21:46:02 +0530

    Rewrite of the program and README
    rewrite of the program for a cleaner menu
    rewrite the program with install instructions

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Aniget
 
-This is program I made for myself to watch and download my favourite anime
+A script which I am writing for me to watch animes without sities going down
+
+## Requirements
+
+- glibcc
+- fzf
+
+## Installation
+
+1. Clone the repo
+
+2. Install the dependecies
+    ```pip install -r requirements.txt```
+
+3. Make a anime directory in the same directory with anime-downloader
+
+4. Run anime-downloader.py
+    ```python3 anime-downloader.py```
+
+# Don't run anime-downloader-rewrite.py it is still development
+
+### Rewrite
+
+I started the rewrite because I wanted to have a clean seach menu, the rewrite is going to take time because I am still learning
---
+++ anime-downloader-rewrite.py
@@ -0,0 +1,55 @@
+import os
+import requests
+import subprocess
+from bs4 import BeautifulSoup
+from pyfzf.pyfzf import FzfPrompt
+
+os.system('clear' if os.name == 'posix' else 'cls')
+
+fzf = FzfPrompt()
+
+base_url = "https://myanimelist.net/"
+
+anime_name = str(input("Enter your anime: ")) 
+
+def search_anime(anime_name):
+    
+    anime_name = anime_name.replace(' ' , '+')
+
+    search_endpoint = f"search/all?q={anime_name}&cat=all"
+
+    search_url = base_url + search_endpoint
+
+    return search_url
+
+def get_animes(search_url):
+    
+    response = requests.get(search_url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    animes_links = soup.find_all("a", href=lambda href: href and href.startswith("https://myanimelist.net/anime/"))
+
+    titles = {
+        "links": [],
+        "titles": [],
+    }
+
+    for i, link in enumerate(animes_links, start=1):
+        url = link["href"].strip()
+        title = link.text.strip()
+        if title == '':
+            continue
+        # print(url)
+        # print(title)
+        else:
+            titles["links"].append(url)
+            titles["titles"].append(title)
+    
+    return titles
+
+search_url = search_anime(anime_name)
+titles = get_animes(search_url)
+# print(titles)
+selected_anime = fzf.prompt(titles["titles"])
+print(selected_anime[0])
